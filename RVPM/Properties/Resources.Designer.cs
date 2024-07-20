223b99a2410520a369e6208352aa5de970a9097e
Dave Andrew Arafol <114577954+DaveHimselff@users.noreply.github.com>
2024-07-20 19:14:26 +0800

    Initial commit

--- RVPM/Properties/Resources.Designer.cs
+++ RVPM/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace RVPM.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -93,59 +93,9 @@ namespace RVPM.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap _111_removebg_preview__1_1 {
-            get {
-                object obj = ResourceManager.GetObject("111-removebg-preview (1)1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _111_removebg_preview__1_2 {
-            get {
-                object obj = ResourceManager.GetObject("111-removebg-preview (1)2", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap _1111_removebg_preview {
             get {
                 object obj = ResourceManager.GetObject("1111-removebg-preview", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _1212_removebg_preview {
-            get {
-                object obj = ResourceManager.GetObject("1212-removebg-preview", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _1222_removebg_preview {
-            get {
-                object obj = ResourceManager.GetObject("1222-removebg-preview", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _1222_removebg_preview__1_ {
-            get {
-                object obj = ResourceManager.GetObject("1222-removebg-preview (1)", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -416,16 +366,6 @@ namespace RVPM.Properties {
         internal static System.Drawing.Bitmap pdficon {
             get {
                 object obj = ResourceManager.GetObject("pdficon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap pdficon2 {
-            get {
-                object obj = ResourceManager.GetObject("pdficon2", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
